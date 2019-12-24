445af6823c911da20102ed63d0ef664b8562a60d
ryanvilbrandt <ryan.v@zonarsystems.com>
2019-12-24 14:46:32 -0800

    Added archive page

---
+++ js/archive.js
@@ -0,0 +1,56 @@
+import { new_lines_to_array } from "./utils.js";
+
+export async function load_archive() {
+    let sections_promise = fetch_archive_sections();
+    let json_list_promise = fetch_all_json_data();
+    let sections = await sections_promise;
+    let json_list = await json_list_promise;
+    build_archive_list(sections, json_list);
+}
+
+async function fetch_archive_sections() {
+    let response = await fetch("./comics/archive_sections.txt");
+    console.log("Fetched archive sections");
+    let text = await response.text();
+    return new_lines_to_array(text);
+}
+
+async function fetch_all_json_data() {
+    let response = await fetch("./comics/directory_list.txt");
+    console.log("Fetched directory list");
+    let text = await response.text();
+    let directory_list = new_lines_to_array(text);
+    console.log(directory_list);
+    let json_dicts = await Promise.all(directory_list.map(i => fetch_json_data(i)));
+    let new_obj = {};
+    for (let i=0; i < json_dicts.length; i++) {
+        new_obj[directory_list[i]] = json_dicts[i];
+    }
+    return new_obj;
+}
+
+async function fetch_json_data(comic_num) {
+    let response = await fetch("./comics/" + comic_num + "/info.json");
+    console.log("Fetched " + comic_num);
+    return response.json();
+}
+
+function build_archive_list(sections, json_list) {
+    let html = "<div id='archive-list'>\n";
+    html += "<ul>\n";
+    sections.forEach(section => {
+        html += "    <li>" + section + "\n";
+        html += "    <ul>";
+        Object.keys(json_list).forEach(key => {
+            let comic = json_list[key];
+            if (comic["tags"].includes(section)) {
+                html += "        <li><a href='index?id=" + key + "'>" + comic["title"] + "</a>";
+                html += " -- " + comic["post_date"] +"</li>";
+            }
+        });
+        html += "    </ul></li>";
+    });
+    html += "</ul>";
+    html += "</div>";
+    document.getElementById("archives").innerHTML = html;
+}
