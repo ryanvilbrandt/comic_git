a23ccc1649808527fe0fb3501dc5fca474ee1293
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2020-07-12 15:49:10 -0700

    Add HTTPS link creation option

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -6,13 +6,13 @@ def get_comic_url(comic_info: RawConfigParser):
     comic_domain, base_directory = None, ""
     if os.path.isfile("CNAME"):
         with open("CNAME") as f:
-            comic_domain = f.read().rstrip('/')
+            comic_domain = f.read().strip('/')
     elif "GITHUB_REPOSITORY" in os.environ:
         repo_author, base_directory = os.environ["GITHUB_REPOSITORY"].split("/")
-        comic_domain = f"http://{repo_author}.github.io"
+        comic_domain = f"{repo_author}.github.io"
     else:
         if comic_info.has_option("Comic Settings", "Comic domain"):
-            comic_domain = comic_info.get("Comic Settings", "Comic domain").rstrip("/")
+            comic_domain = comic_info.get("Comic Settings", "Comic domain").strip("/")
         else:
             raise ValueError(
                 'Set "Comic domain" in the [Comic Settings] section of your comic_info.ini file '
@@ -20,7 +20,14 @@ def get_comic_url(comic_info: RawConfigParser):
             )
         if comic_info.has_option("Comic Settings", "Comic subdirectory"):
             base_directory = comic_info.get("Comic Settings", "Comic subdirectory").strip("/")
+    if not comic_domain.startswith("http"):
+        if (comic_info.has_option("Comic Settings", "Use https when building comic URL") and
+                comic_info.getboolean("Comic Settings", "Use https when building comic URL")):
+            comic_domain = "https://" + comic_domain
+        else:
+            comic_domain = "http://" + comic_domain
     if base_directory:
         base_directory = "/" + base_directory
     comic_url = comic_domain + base_directory
+    print((comic_url, base_directory))
     return comic_url, base_directory
--- src/templates/base.tpl
+++ src/templates/base.tpl
@@ -33,7 +33,7 @@
     <meta property="og:title" content="{{ comic_title }}" />
     <meta property="og:description" content="{{ comic_description }}" />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="{{ comic_url }}" />
+    <meta property="og:url" content="{{ comic_url }}/" />
     <meta property="og:image" content="{{ comic_url + '/your_content/images/preview_image.png' }}" />
     <meta property="og:image:width" content="100px" />
     <meta property="og:image:height" content="100px" />
