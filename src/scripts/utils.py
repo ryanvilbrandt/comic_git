c1dad2b188391ce3974e7b77b66e6d2f5a65a871
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2020-07-12 15:03:31 -0700

    Added CNAME support

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -3,20 +3,23 @@ from configparser import RawConfigParser
 
 
 def get_comic_url(comic_info: RawConfigParser):
-    comic_domain, base_directory = None, None
-    if "GITHUB_REPOSITORY" in os.environ:
+    comic_domain, base_directory = None, ""
+    if os.path.isfile("CNAME"):
+        with open("CNAME") as f:
+            comic_domain = f.read().rstrip('/')
+    elif "GITHUB_REPOSITORY" in os.environ:
         repo_author, base_directory = os.environ["GITHUB_REPOSITORY"].split("/")
         comic_domain = f"http://{repo_author}.github.io"
-    if comic_info.has_option("Comic Info", "Comic domain"):
-        comic_domain = comic_info.get("Comic Info", "Comic domain").rstrip("/")
-        base_directory = ""
-    if comic_info.has_option("Comic Info", "Comic subdirectory"):
-        base_directory = comic_info.get("Comic Info", "Comic subdirectory").strip("/")
-    if not comic_domain:
-        raise ValueError(
-            'Set "Comic domain" in the [Comic Info] section of your comic_info.ini file '
-            'before building your site locally. Please see the comic_git wiki for more information.'
-        )
+    else:
+        if comic_info.has_option("Comic Settings", "Comic domain"):
+            comic_domain = comic_info.get("Comic Settings", "Comic domain").rstrip("/")
+        else:
+            raise ValueError(
+                'Set "Comic domain" in the [Comic Settings] section of your comic_info.ini file '
+                'before building your site locally. Please see the comic_git wiki for more information.'
+            )
+        if comic_info.has_option("Comic Settings", "Comic subdirectory"):
+            base_directory = comic_info.get("Comic Settings", "Comic subdirectory").strip("/")
     if base_directory:
         base_directory = "/" + base_directory
     comic_url = comic_domain + base_directory
