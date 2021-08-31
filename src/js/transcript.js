e3b837d243443de3925547dc159d1713c314f5ba
Ryan Vilbrandt <ryan.v@zonarsystems.com>
2021-08-31 11:53:56 -0700

    Don't show language selector when there's only one transcript

--- src/js/transcript.js
+++ src/js/transcript.js
@@ -3,10 +3,16 @@ export function init() {
     let languageList = document.getElementById("language-select");
     let transcriptList = document.getElementById("active-transcript");
     let transcripts = transcriptList.children;
+    transcripts[0].style.display = "block";
+    
+    if (!languageList) {
+        // If the language list was not built into this page (i.e. there was only one transcript to display), 
+        // then don't set up event listeners for the language list
+        return;
+    }
+    
     languageList.selectedIndex = 0;
-    transcripts[languageList.selectedIndex].style.display = "block";
-
-    languageList.addEventListener("click",function() {
+    languageList.addEventListener("click", function() {
         for (let i = 0; i < transcripts.length; i++) {
             transcripts[i].style.display = i === languageList.selectedIndex ? "block" : null;
         }
