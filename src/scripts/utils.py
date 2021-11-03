270de2d4d975fd9034e788cfffed2850f6c287f8
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2021-11-03 16:21:50 -0700

    Reorganize get_comic_url

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -9,25 +9,31 @@ jinja_environment = None
 
 def get_comic_url(comic_info: RawConfigParser):
     comic_domain, base_directory = None, ""
-    if os.path.isfile("CNAME"):
+    # Let user-defined comic domain and base directory override all other values
+    if comic_info.has_option("Comic Settings", "Comic domain"):
+        comic_domain = comic_info.get("Comic Settings", "Comic domain").strip("/")
+    if comic_info.has_option("Comic Settings", "Comic subdirectory"):
+        base_directory = comic_info.get("Comic Settings", "Comic subdirectory").strip("/")
+    # If we have a CNAME file, use that for the comic domain
+    if not comic_domain and os.path.isfile("CNAME"):
         with open("CNAME") as f:
             comic_domain = f.read().strip('/')
-    elif "GITHUB_REPOSITORY" in os.environ:
-        repo_author, base_directory = os.environ["GITHUB_REPOSITORY"].split("/")
-        comic_domain = f"{repo_author}.github.io"
-        if base_directory.lower() == f"{repo_author.lower()}.github.io":
-            # In this case, Github will try to deploy to http://<username>.github.io/ so we should unset base_directory
-            base_directory = ""
-    else:
-        if comic_info.has_option("Comic Settings", "Comic domain"):
-            comic_domain = comic_info.get("Comic Settings", "Comic domain").strip("/")
-        else:
-            raise ValueError(
-                'Set "Comic domain" in the [Comic Settings] section of your comic_info.ini file '
-                'before building your site locally. Please see the comic_git wiki for more information.'
-            )
-        if comic_info.has_option("Comic Settings", "Comic subdirectory"):
-            base_directory = comic_info.get("Comic Settings", "Comic subdirectory").strip("/")
+    # If this is running in GitHub and the domain and base directory were not user-defined, derive them here
+    if "GITHUB_REPOSITORY" in os.environ:
+        repo_author, repo_name = os.environ["GITHUB_REPOSITORY"].split("/")
+        if not comic_domain:
+            comic_domain = f"{repo_author}.github.io"
+        if not base_directory:
+            base_directory = repo_name
+            if base_directory.lower() == f"{repo_author.lower()}.github.io":
+                # In this case, Github will try to deploy to http://<username>.github.io/ so we unset base_directory
+                base_directory = ""
+    # Helpful error for dumb schmucks trying to build locally for the first time
+    if not comic_domain:
+        raise ValueError(
+            'Set "Comic domain" in the [Comic Settings] section of your comic_info.ini file '
+            'before building your site locally. Please see the comic_git wiki for more information.'
+        )
     if not comic_domain.startswith("http"):
         if (comic_info.has_option("Comic Settings", "Use https when building comic URL") and
                 comic_info.getboolean("Comic Settings", "Use https when building comic URL")):
