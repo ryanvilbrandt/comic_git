ad75b5ec4f5d012efec6d77d554e4015e57952a5
Ryan Vilbrandt <ryan.v@zonarsystems.com>
2022-02-03 17:12:35 -0800

    Add additional import to delete_autogenerated_files.py

--- src/scripts/delete_autogenerated_files.py
+++ src/scripts/delete_autogenerated_files.py
@@ -1,4 +1,8 @@
-from src.scripts.build_site import delete_output_file_space
+try:
+    from src.scripts.build_site import delete_output_file_space
+except ImportError:
+    # Some people have issues with the above import. Try this one as well, just to see if it works.
+    from build_site import delete_output_file_space
 from utils import find_project_root
 
 find_project_root()
